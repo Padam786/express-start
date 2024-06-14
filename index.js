6cb94d3b509b932381fd3658da88e3c9972a071e
Padam7890 <padamthapamoho@gmail.com>
2024-06-14 20:46:55 +0545

    new

--- index.js
+++ index.js
@@ -47,30 +47,30 @@ app.put("/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   //check  user id is match with params id
-  const index = users.findIndex((item) => item.id === id);
+  const indexItem = users.find((item) => item.id === id);
 
   //if id not match with params id
-  if (index === -1) {
+  if (!indexItem) {
     return res.status(404).json({
       message: "item not found",
     });
-  }
+  } else {
+    //get item from cilent / body and update it to index
+    users[indexItem] = { id, ...req.body };
 
-  //get item from cilent / body and update it to index
-  users[index] = { id, ...req.body };
+    //write a  item in file
+    fs.writeFile("MOCK_DATA.json", JSON.stringify(users), (writeErr) => {
+      if (writeErr) {
+        return res.status(500).json({ message: "Error writing file" });
+      }
 
-  //write a  item in file
-  fs.writeFile("MOCK_DATA.json", JSON.stringify(users, null, 2), (writeErr) => {
-    if (writeErr) {
-      return res.status(500).json({ message: "Error writing file" });
-    }
-
-    //send response if evrrything is ok
-    res.json({
-      message: "Item updated successfully",
-      data: users[index],
+      //send response if evrrything is ok
+      res.json({
+        message: "Item updated successfully",
+        data: users[indexItem],
+      });
     });
-  });
+  }
 });
 
 //for data delete, use delete method / Homework
