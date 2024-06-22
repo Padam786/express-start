d9c3a365811324559174550f0c08a0737dc56afc
Padam7890 <padamthapamoho@gmail.com>
2024-06-22 18:57:07 +0545

    added create

--- config/prisma.js
+++ config/prisma.js
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+const {PrismaClient} = require("@prisma/client")
 
-const prisma = new PrismaClient();
+const prisma  = new PrismaClient();
 
 module.exports = prisma;
+
--- index.js
+++ index.js
@@ -1,27 +1,79 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 //for json support middlware
 app.use(express.json());
 
 //for urlencoded support or object support middlewar
 app.use(express.urlencoded({ extended: true }));
 
-//we are making post
-// title     String
-// content   String?
-// published Boolean @default(false)
-// user_id Int //1
-
-//To create a new post using post method
-app.post("/", (req, res) => {
-  const { title, content, published, user_id } = req.body;
-
-  //to be continue tomorrow
+const prisma = require("./config/prisma");
 
 
+//homework for you . 
+
+app.post("/user", async(req,res)=>{
+  try {
+    //get from frontend
+
+    //validation
+
+    //database ma save garne
+
+    //send responnse to frontend
+  
+  } catch (error) {
+
+    //error handle using status 500
+
+  }
+
+})
+
+
+
+
+
+app.post("/", async (req, res) => {
+  ///get data from body-manidatory
+  try {
+    const { title, content, published, user_id } = req.body;
+
+    //validation /optional
+
+    if (!title) {
+      return res.status(404).json({
+        message: "Invalid title",
+      });
+    }
+
+    //to save data in database
+    const saveData = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        published: published,
+        user_id: parseInt(user_id),
+      },
+    });
+
+    return res.status(201).json({
+      message: "Post saved successfully",
+      data: saveData,
+    });
+  } catch (error) {
+    console.log(error) ///debuging 
+    return res.status(500).json({
+      message: " Sever error",
+      error: error,
+    });
+  }
 });
+
+
+
+
+
 
 //listening port
 
