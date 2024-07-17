4e468d66309ed39bebd0b5ed688a8ae00b7e1bfe
Padam786 <padamthapamaho@gmail.com>
2024-07-17 17:23:05 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express();
 const cors = require("cors");
 const port = 3000;
 
+app.use(cors());
+
+//for json support middlware
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+})),
+
+groupRoutes(app) // register routes
+
+// middleware to authenticate requests
+
+
 //listening port
 
 app.listen(port, () => {
